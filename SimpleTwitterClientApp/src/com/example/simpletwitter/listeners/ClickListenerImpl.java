9c3f966fd2c7f7fc8ec5928f82632520ce6af198
Elancheran Subramanian <cheran@notedieeast-lm.(none)>
2014-07-04 12:05:50 -0700

    Added the User Profile Activity View Support

---
+++ SimpleTwitterClientApp/src/com/example/simpletwitter/listeners/ClickListenerImpl.java
@@ -0,0 +1,13 @@
+package com.example.simpletwitter.listeners;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ClickListenerImpl implements  OnClickListener{
+
+	@Override
+	public void onClick(View v) {
+		
+	}
+
+}
